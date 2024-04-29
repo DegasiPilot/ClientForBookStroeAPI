db5891d4a8eac07d459618987f0c940c0a176bcf
DegasiPilot <timurnig@gmail.com>
2024-04-29 19:26:42 +0300

    Добавьте файлы проекта.

---
+++ APIAgent.cs
@@ -0,0 +1,82 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Windows;
+
+namespace ClientForMyAPI
+{
+    internal class APIAgent
+    {
+        HttpClient client = new HttpClient();
+        private readonly string url = @"https://localhost:7125/";
+
+        public IEnumerable<Book> GetBooks()
+        {
+            return ConvertFromJson<IEnumerable<Book>>(MakeGetRequestAsync(url + "books"));
+        }
+
+        public IEnumerable<Book> GetBookId(string id)
+        {
+            return ConvertFromJson<IEnumerable<Book>>(MakeGetRequestAsync(url + $"book/{id}"));
+        }
+
+        public void AddBook(Book book)
+        {
+            MakePostRequestAsync(url + "book", book);
+        }
+
+        public void PutBook(Book book)
+        {
+            var status = MakePutRequestAsync(url + "book", book);
+            if(status == HttpStatusCode.NotFound)
+            {
+                MessageBox.Show("Этой книги не существует!");
+            }
+        }
+
+        public void DeleteBook(int id)
+        {
+            MessageBox.Show(MakeDeleteRequestAsync(url + $"book", id).ToString());
+        }
+        private T ConvertFromJson<T>(string json)
+        {
+            return JsonConvert.DeserializeObject<T>(json);
+        }
+        private string MakePostRequestAsync(string url, Book book)
+        {
+            var content = new StringContent(JsonConvert.SerializeObject(book), Encoding.UTF8, "application/json");
+            var response = client.PostAsync(url, content).Result;
+            var httpStatusCode = response.StatusCode;
+            if (httpStatusCode != HttpStatusCode.OK)
+            {
+                return "";
+            }
+            return response.Content.ReadAsStringAsync().Result;
+        }
+        private string MakeGetRequestAsync(string url)
+        {
+            var response = client.GetAsync(url).Result;
+            var httpStatusCode = response.StatusCode;
+            if (httpStatusCode != HttpStatusCode.OK)
+            {
+                return "";
+            }
+            return response.Content.ReadAsStringAsync().Result;
+        }
+
+        private HttpStatusCode MakePutRequestAsync(string url, Book book)
+        {
+            var content = new StringContent(JsonConvert.SerializeObject(book), Encoding.UTF8, "application/json");
+            var response = client.PutAsync(url, content).Result;
+            return response.StatusCode;
+        }
+        private HttpStatusCode MakeDeleteRequestAsync(string url, int id)
+        {
+            var response = client.DeleteAsync(url + $"/{id}").Result;
+            return response.StatusCode;
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ClientForMyAPI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private APIAgent APIAgent = new();
+
+         public MainWindow()
+        {
+            InitializeComponent();
+            BooksDataGrid.ItemsSource = APIAgent.GetBooks();
+        }
+
+        private void bookIdTB_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(bookIdTB.Text)) 
+            { 
+                BooksDataGrid.ItemsSource = null;
+                BooksDataGrid.ItemsSource = APIAgent.GetBooks();
+            }
+            else
+            {
+                BooksDataGrid.ItemsSource = null;
+                var books = APIAgent.GetBooks().Where(x => x.Title.ToLower().Contains(bookIdTB.Text.ToLower()));
+                BooksDataGrid.ItemsSource = books;
+            }
+        }
+
+        private void DeleteBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if(BooksDataGrid.SelectedItem == null)
+            {
+                MessageBox.Show("Сначала нажмите на строку которую хотите удалить!");
+                return;
+            }
+            var selectedBook = (BooksDataGrid.SelectedItem as Book);
+            APIAgent.DeleteBook(selectedBook.Id);
+            bookIdTB_TextChanged(null, null);
+            ClearInfoInTextBoxes();
+        }
+
+        private void AddBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (TryGetBookFromTextBoxes(out Book book))
+            {
+                APIAgent.AddBook(book);
+                bookIdTB_TextChanged(null, null);
+                ClearInfoInTextBoxes();
+            }
+        }
+
+        private void BooksDataGrid_SelectedCellsChanged(object sender, SelectedCellsChangedEventArgs e)
+        {
+            if (e.AddedCells.Any())
+            {
+                UpdateInfoInTextBoxes(e.AddedCells[0].Item as Book);
+            }
+        }
+
+        private void UpdateInfoInTextBoxes(Book book)
+        {
+            IdTb.Text = book.Id.ToString();
+            TitleTb.Text = book.Title;
+            AuthorTb.Text = book.Author;
+            PagesCountTb.Text = book.PagesCount.ToString();
+            PriceTb.Text = book.Price.ToString();
+        }
+
+        private void ClearInfoInTextBoxes()
+        {
+            IdTb.Clear();
+            TitleTb.Clear();
+            AuthorTb.Clear();
+            PagesCountTb.Clear();
+            PriceTb.Clear();
+        }
+
+        private void ChangeBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if(TryGetBookFromTextBoxes(out Book book))
+            {
+                if (int.TryParse(IdTb.Text, out int id))
+                {
+                    book.Id = id;
+                    APIAgent.PutBook(book);
+                    bookIdTB_TextChanged(null, null);
+                    ClearInfoInTextBoxes();
+                }
+                else
+                {
+                    MessageBox.Show("Вы не выбрали книгу для редактирования!");
+                }
+            }
+        }
+
+        private bool TryGetBookFromTextBoxes(out Book book)
+        {
+            if (string.IsNullOrEmpty(TitleTb.Text))
+            {
+                MessageBox.Show("Введите название книги!");
+                book = new Book();
+                return false;
+            }
+
+            book = new Book()
+            {
+                Title = TitleTb.Text,
+                Author = AuthorTb.Text,
+            };
+
+            if (int.TryParse(PagesCountTb.Text, out int pagesCount))
+            {
+                book.PagesCount = pagesCount;
+            }
+            else
+            {
+                MessageBox.Show("Неправильное значение в поле Pages count");
+                return false;
+            }
+            if (decimal.TryParse(PriceTb.Text, out decimal price))
+            {
+                book.Price = price;
+            }
+            else
+            {
+                MessageBox.Show("Неправильное значение в поле Price");
+                return false;
+            }
+            return true;
+        }
+    }
+}
